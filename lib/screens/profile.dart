5a9faa7d0b219aa5510dcbc557114b5f75f1e5e3
Izzul Faiz <izzulfaiz@users.noreply.github.com>
2021-12-22 09:37:33 +0700

    revisi

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,4 +1,8 @@
+
+
 import 'package:flutter/material.dart';
+import 'package:login_page_1/main.dart';
+import 'package:login_page_1/screens/about_us.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -31,22 +35,68 @@ class ProfilePage extends StatelessWidget {
             children: [
               CircleAvatar(
                 radius: 80.0,
+                backgroundImage:
+                    AssetImage("assets/images/avatar/icon_register.png"),
                 backgroundColor: Colors.white,
               ),
               SizedBox(
-                height: 20.0,
+                height: 50.0,
               ),
               ListTile(
-                title: Text("Nama",
+                title: Text("Username",
                     style: TextStyle(
                         fontFamily: 'Montserrat',
                         fontSize: 20.0,
                         fontWeight: FontWeight.bold)),
                 subtitle: Text(
-                  'Hasif Izzul Faiz',
+                  mainaccount[0].toString(),
                   style: TextStyle(fontFamily: 'Montserrat', fontSize: 15.0),
                 ),
-              )
+              ),
+              ListTile(
+                title: Text("No Telepon",
+                    style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold)),
+                subtitle: Text(
+                  mainaccount[2].toString(),
+                  style: TextStyle(fontFamily: 'Montserrat', fontSize: 15.0),
+                ),
+              ),
+              ListTile(
+                title: Text("Alamat",
+                    style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold)),
+                subtitle: Text(
+                  mainaccount[3].toString(),
+                  style: TextStyle(fontFamily: 'Montserrat', fontSize: 15.0),
+                ),
+              ),
+              SizedBox(
+                width: 10.0,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 40.0),
+                child: Row(
+                  children: [
+                    ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return AboutUs();
+                          }));
+                        },
+                        child: Text(
+                          'About Us',
+                          style: TextStyle(
+                              fontFamily: 'Montserrat', fontSize: 15.0),
+                        ))
+                  ],
+                ),
+              ),
             ],
           ),
         ),
